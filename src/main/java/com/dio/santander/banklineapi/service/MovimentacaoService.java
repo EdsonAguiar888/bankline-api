9fb4f65f0147ad8073fde0b9b5f978b25044314b
Edson Aguiar <edsonaguiar888@gmail.com>
2022-05-07 11:47:27 -0300

    Atualização final do projeto Spring Boot API

--- src/main/java/com/dio/santander/banklineapi/service/MovimentacaoService.java
+++ src/main/java/com/dio/santander/banklineapi/service/MovimentacaoService.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dio.santander.banklineapi.dto.NovaMovimentacao;
+import com.dio.santander.banklineapi.model.Correntista;
 import com.dio.santander.banklineapi.model.Movimentacao;
 import com.dio.santander.banklineapi.model.MovimentacaoTipo;
+import com.dio.santander.banklineapi.repository.CorrentistaRepository;
 import com.dio.santander.banklineapi.repository.MovimentacaoRepository;
 
 @Service
@@ -15,6 +17,11 @@ public class MovimentacaoService {
 	
 	@Autowired
 	private MovimentacaoRepository repository;
+	
+	@Autowired
+	private CorrentistaRepository correntistaRepository;
+	
+	
 	
 	public void save(NovaMovimentacao novaMovimentacao) {
 		
@@ -31,6 +38,12 @@ public class MovimentacaoService {
 		movimentacao.setIdConta(novaMovimentacao.getIdConta());
 		movimentacao.setTipo(novaMovimentacao.getTipo());
 		movimentacao.setValor(valor);
+		
+		Correntista correntista = correntistaRepository.findById(novaMovimentacao.getIdConta()).orElse(null);
+		if(correntista != null) {
+			correntista.getConta().setSaldo(correntista.getConta().getSaldo() + valor);
+			correntistaRepository.save(correntista);
+		}
 		
 		repository.save(movimentacao);
 		
